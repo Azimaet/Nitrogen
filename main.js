6748c13c153c9a14155029ecd8899766eaae54bb
Azimaet <maximedolo@gmail.com>
2019-05-04 00:29:36 +0200

    Keep logs wip

--- main.js
+++ main.js
@@ -3,9 +3,16 @@
 
 plannedDepth = 26;
 explorationTime = 50;
-startTime = 11.00;
+startTime = 11.03;
 
 var userDive = new Dive(plannedDepth, explorationTime, startTime);
 
 
 console.log(userDive);
+console.log(
+    "-La plongée démarre à " + startTime + "." + "\r\n" +
+    "-La profondeur max est de " + plannedDepth + "m. \r\n" +
+    "-La durée d'exploration est de " + explorationTime + "mn, et la durée de remontée sera de " + userDive.ascentTime +"mn. \r\n" +
+    "-La durée totale de la plongée sera donc de " + userDive.totalTime + "mn. \r\n" + 
+    "-Le groupe de saturation des tissus sera de " + userDive.matchedTime.gps + "\r\n"
+);
